62a16cfe1c69092a80d45ff9e2b032a68bf3a8f5
Kino <jose33201@hotmail.com>
2018-01-23 23:15:35 -0500

    Made Glacium more deadly

--- src/main/java/com/legacy/glacidus/blocks/natural/BlockGlaciumStatic.java
+++ src/main/java/com/legacy/glacidus/blocks/natural/BlockGlaciumStatic.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -24,6 +24,13 @@ public class BlockGlaciumStatic extends BlockFluidClassic
 	{
 		super(BlocksGlacidus.GLACIUM, GlacidusMaterial.GLACIUM);
 	}
+
+	@Override
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn)
+    {
+		entityIn.motionY += 0.5F;
+    	entityIn.attackEntityFrom(new DamageSource("glacium_frosted"), 7.0F);
+    }
 
     @SideOnly(Side.CLIENT)
     public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand)
