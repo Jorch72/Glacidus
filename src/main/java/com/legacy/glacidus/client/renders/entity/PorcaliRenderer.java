f6fc5b55973cd42ba1d2c629380d0459c39d5f8d
KingPhygieBoo <wheresperry41@gmail.com>
2018-01-25 00:43:06 -0600

    Why didn't this go with that commit?

---
+++ src/main/java/com/legacy/glacidus/client/renders/entity/PorcaliRenderer.java
@@ -0,0 +1,39 @@
+package com.legacy.glacidus.client.renders.entity;
+
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.util.ResourceLocation;
+
+import com.legacy.glacidus.client.models.entity.ModelPorcali;
+import com.legacy.glacidus.entities.passive.EntityPorcali;
+
+public class PorcaliRenderer extends RenderLiving<EntityPorcali>
+{
+
+	private static final ResourceLocation TEXTURE = new ResourceLocation("glacidus", "textures/entity/porcali/porcali.png");
+
+	public PorcaliRenderer(RenderManager rendermanagerIn)
+	{
+		super(rendermanagerIn, new ModelPorcali(), 0.7F);
+	}
+	
+	protected void scalePorc(EntityPorcali porc)
+	{
+
+		GlStateManager.scale(1.3F, 1.3F, 1.3F);
+	}
+
+	@Override
+	protected void preRenderCallback(EntityPorcali entityliving, float f)
+	{
+		this.scalePorc(entityliving);
+	}
+
+	@Override
+	protected ResourceLocation getEntityTexture(EntityPorcali entity)
+	{
+		return TEXTURE;
+	}
+
+}
