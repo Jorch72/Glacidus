680479576d97485386d5666d8720d28e5e915315
KingPhygieBoo <wheresperry41@gmail.com>
2018-01-27 00:41:28 -0600

    New creative tabs!

--- src/main/java/com/legacy/glacidus/creativetab/GlacidusCreativeTabs.java
+++ src/main/java/com/legacy/glacidus/creativetab/GlacidusCreativeTabs.java
@@ -1,6 +1,7 @@
 package com.legacy.glacidus.creativetab;
 
 import com.legacy.glacidus.blocks.BlocksGlacidus;
+import com.legacy.glacidus.items.ItemsGlacidus;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
@@ -14,6 +15,22 @@ public class GlacidusCreativeTabs
 		public ItemStack getTabIconItem() 
 		{
 			return new ItemStack(BlocksGlacidus.lumicia_grass);
+		}
+	};
+	public static final CreativeTabs ITEMS = new CreativeTabs("glacidus.items.tab")
+	{
+		@Override
+		public ItemStack getTabIconItem() 
+		{
+			return new ItemStack(ItemsGlacidus.grapes);
+		}
+	};
+	public static final CreativeTabs TOOLS = new CreativeTabs("glacidus.tools.tab")
+	{
+		@Override
+		public ItemStack getTabIconItem() 
+		{
+			return new ItemStack(ItemsGlacidus.eukeite_hoe);
 		}
 	};
 
