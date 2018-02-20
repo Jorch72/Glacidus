df53f391a568e6cd239e5f2c18e7ee859a6291c9
KingPhygieBoo <wheresperry41@gmail.com>
2018-02-20 00:57:19 -0600

    Crysium sounds like glass again.

--- src/main/java/com/legacy/glacidus/blocks/natural/BlockCrysiumOre.java
+++ src/main/java/com/legacy/glacidus/blocks/natural/BlockCrysiumOre.java
@@ -20,7 +20,7 @@ public class BlockCrysiumOre extends Block
 
 		this.setHardness(3.0F);
 		this.setResistance(5.0F);
-		this.setSoundType(SoundType.STONE);
+		this.setSoundType(SoundType.GLASS);
 		this.setLightLevel(0.7F);
 	}
 	
